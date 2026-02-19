cf2d830fa57b3e158efdf40b8c17e0cccec6e5b8
BLadner34 <brittnee.hallman@yahoo.com>
2026-02-18 20:30:46 -0600

    feat: adding app js

---
+++ app.js
@@ -0,0 +1,9 @@
+// const firstName, let score, const isActive, const tags = ['js','week3']//
+const person = {firstName: "brittnee"};
+let score = 34; 
+const isActive; 
+const user = {
+    id: 1, 
+    favoriteColor: "blue", 
+}
+console.log(user[favoriteColor]); 
